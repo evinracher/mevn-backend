7ba1acdd6837c4f986c6faeafc119aed3a421f3c
evinracher <eevinley@gmail.com>
2021-01-09 20:39:40 -0500

    Init

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+
+// Middleware
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// ROUTES:
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// Middleware for Vue.js router history mode
+const history = require('connect-history-api-fallback');
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), () => {
+  console.log('Example app listening on port '+ app.get('port'));
+});
